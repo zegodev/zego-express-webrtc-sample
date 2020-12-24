0c17bb06f59393a70da1f5a094139fbb6b645bc8
haleycai <yytm0112@163.com>
2020-12-24 18:09:38 +0800

    active

--- src/base/index.js
+++ src/base/index.js
@@ -146,18 +146,18 @@ $(async () => {
         })
     })
 
-    // $('#mutePlayStreamVideo').click(function() {
-    //     useLocalStreamList.forEach(item => {
-    //         zg.mutePlayStreamVideo(item.streamID, !$(this).hasClass('disabled'));
-    //     })
-    //     $(this).toggleClass('disabled');
-    // })
-    // $('#mutePlayStreamAudio').click(function() {
-    //     useLocalStreamList.forEach(item => {
-    //         zg.mutePlayStreamAudio(item.streamID, !$(this).hasClass('disabled'));
-    //     })
-    //     $(this).toggleClass('disabled');
-    // })
+    $('#mutePlayStreamVideo').click(function() {
+        useLocalStreamList.forEach(item => {
+            zg.mutePlayStreamVideo(item.streamID, !$(this).hasClass('disabled'));
+        })
+        $(this).toggleClass('disabled');
+    })
+    $('#mutePlayStreamAudio').click(function() {
+        useLocalStreamList.forEach(item => {
+            zg.mutePlayStreamAudio(item.streamID, !$(this).hasClass('disabled'));
+        })
+        $(this).toggleClass('disabled');
+    })
     $('#tcpOnly').change((e) => {
         // console.error(e.target.value);
         const tcpOnly = e.target.value;
